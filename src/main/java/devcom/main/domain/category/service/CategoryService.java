a94e4639a42a05ec1fdbeb367c2b51f65b9da89d
twinogre <glad2you@naver.com>
2024-01-19 21:22:38 +0900

    Category_controller_service_repository_추가.2

--- src/main/java/devcom/main/domain/category/service/CategoryService.java
+++ src/main/java/devcom/main/domain/category/service/CategoryService.java
@@ -1,5 +1,6 @@
 package devcom.main.domain.category.service;
 
+import devcom.main.domain.category.entity.Category;
 import devcom.main.domain.category.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,27 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+
+    //category create
+    public void create(String categoryName){
+        Category category = Category.builder()
+                .categoryName(categoryName)
+                .build();
+
+        this.categoryRepository.save(category);
+    }
+
+    //category modify
+    public void modify(Category category, String categoryName){
+       Category category1 = category.toBuilder()
+               .categoryName(categoryName)
+               .build();
+
+       this.categoryRepository.save(category1);
+    }
+
+    //category delete
+    public void delete(Category category){
+        this.categoryRepository.delete(category);
+    }
 }
