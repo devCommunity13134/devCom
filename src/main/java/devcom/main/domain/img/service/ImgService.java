0fd3f3764099bd20bdb66d840232bbbbc0f31eea
twinogre <glad2you@naver.com>
2024-01-21 12:05:49 +0900

    img_controller_service_repository_추가

---
+++ src/main/java/devcom/main/domain/img/service/ImgService.java
@@ -0,0 +1,36 @@
+package devcom.main.domain.img.service;
+
+import devcom.main.domain.category.entity.Category;
+import devcom.main.domain.img.entity.Img;
+import devcom.main.domain.img.repository.ImgRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ImgService {
+    private final ImgRepository imgRepository;
+
+    //category create
+    public void create(String imgUrl){
+        Img img = Img.builder()
+                .img_url(imgUrl)
+                .build();
+
+        this.imgRepository.save(img);
+    }
+
+    //category modify
+    public void modify(Img img, String imgUrl){
+        Img img1 = img.toBuilder()
+                .img_url(imgUrl)
+                .build();
+
+        this.imgRepository.save(img1);
+    }
+
+    //category delete
+    public void delete(Img img){
+        this.imgRepository.delete(img);
+    }
+}
--- src/test/java/devcom/main/MainApplicationTests.java
+++ src/test/java/devcom/main/MainApplicationTests.java
@@ -2,6 +2,7 @@ package devcom.main;
 
 import devcom.main.domain.answer.Service.AnswerService;
 import devcom.main.domain.article.service.ArticleService;
+import devcom.main.domain.img.service.ImgService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,9 +14,19 @@ class MainApplicationTests {
 	ArticleService articleService;
 	@Autowired
 	AnswerService answerService;
+	@Autowired
+	ImgService imgService;
+
 	@Test
 	void contextLoads() {
 		this.articleService.create("s1", "c1");
+	}
+
+	// img test
+	// img create test
+	@Test
+	void imgCreate(){
+		this.imgService.create("https://yt3.ggpht.com/lE7K-Ab6YHP4DzO9SwtFuS-GQxFbkklXcSCp99SFSzXWa6OlGw_esCFX0GUKuBa6DjaCd4-gmxsS=s800-c-fcrop64=1,2c330000d6ddffff-nd-v1");
 	}
 
 }
