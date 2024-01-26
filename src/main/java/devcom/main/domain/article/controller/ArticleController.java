f9f9098e43b06e2f4c47b1563003bd15a9959581
TwinOgre <glad2you@naver.com>
2024-01-26 16:34:29 +0900

    article_create_create_fix

--- src/main/java/devcom/main/domain/article/controller/ArticleController.java
+++ src/main/java/devcom/main/domain/article/controller/ArticleController.java
@@ -1,11 +1,14 @@
 package devcom.main.domain.article.controller;
 
 
+import devcom.main.domain.article.ArticleForm;
 import devcom.main.domain.article.entity.Article;
 import devcom.main.domain.article.repository.ArticleRepository;
 import devcom.main.domain.article.service.ArticleService;
 import devcom.main.domain.category.entity.Category;
 import devcom.main.domain.category.service.CategoryService;
+import devcom.main.domain.user.entity.SiteUser;
+import devcom.main.domain.user.service.UserService;
 import devcom.main.global.rq.Rq;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +16,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -27,9 +29,10 @@ public class ArticleController {
 
     private final ArticleService articleService;
     private final CategoryService categoryService;
+    private final UserService userService;
 
     // article list
-    @GetMapping("/list/{category}")
+    @GetMapping("/{category}")
     public String articleList(Model model, @PathVariable("category") String category, @RequestParam(value = "page",defaultValue = "0") int page){
         Category category1 = this.categoryService.getCategory(category);
         Page<Article> paging = this.articleService.getArticleList(page,category1);
@@ -43,10 +46,25 @@ public class ArticleController {
 
         return "article/form";
     }
-    // detail getMapping_fix_need
-    @GetMapping("/detail/1")
-    public String articleDetail(){
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/create")
+    public String articleCreate(@Valid ArticleForm articleForm, BindingResult bindingResult, Principal principal){
+        if(bindingResult.hasErrors()){
+            return "article/form";
+        }
+        Category category = this.categoryService.getCategory(articleForm.getCategoryName());
+        SiteUser author = this.userService.findByUsername(principal.getName());
 
+        this.articleService.create(category ,articleForm.getSubject(),articleForm.getContent(),author);
+
+        return String.format("redirect:/article/%s",articleForm.getCategoryName());
+    }
+
+    // detail getMapping_fix_need
+    @GetMapping("/detail/{id}")
+    public String articleDetail(Model model, @PathVariable("id") Long id){
+        Article article = this.articleService.getArticle(id);
+        model.addAttribute("article",article);
         return "article/detail";
     }
 }
